39fcc05d6567909cbad3a4f9282379eb12daec6f
mbedoya <mauricio.bedoya@gmail.com>
2015-04-28 14:53:07 -0500

    test for login

--- www/js/controllers-login.js
+++ www/js/controllers-login.js
@@ -1,85 +1,89 @@
 moduloControlador.controller('LoginCtrl', function($scope, $rootScope, $ionicLoading, $ionicPopup, $state, $http, $filter, $ionicHistory, Mama, Internet, GA, Utilidades) {
 
-       //Registro en Analytics      
-       GA.trackPage($rootScope.gaPlugin, "Inicio de sesión");
-       
-       $scope.mostrarAyuda = function(titulo, mensaje) {
-           var alertPopup = $ionicPopup.alert({
-             title: titulo,
-             template: mensaje
-           });
-         };
-         
-    	$scope.datosInicio = {cedula: '' };
-    
-    	//Autenticar a la Mamá Empresaria
-        $scope.capturarCedula = function() {
+    //Registro en Analytics
+    GA.trackPage($rootScope.gaPlugin, "Inicio de sesión");
 
-            alert("done");
+    $scope.mostrarAyuda = function(titulo, mensaje) {
+        var alertPopup = $ionicPopup.alert({
+            title: titulo,
+            template: mensaje
+        });
+    };
 
-            $rootScope.datos = { cedula: $scope.datosInicio.cedula }
-            
-            //Cédula vacía
-            if(!$rootScope.datos.cedula){
-                $scope.mostrarAyuda("Inicio de sesión","Ingresa tu cédula");
-                return;
-            }
+    $scope.datosInicio = {cedula: '' };
 
-            //Cantidad de caracteres
-            if(String($rootScope.datos.cedula).length < 6 || String($rootScope.datos.cedula).length > 10){
-                $scope.mostrarAyuda("Inicio de sesión","Ingresa entre 6 y 10 dígitos");
-                return;
-            }
+    $scope.test = function(){
+        alert("Test");
+    }
 
-            //Caracteres especiales
-            if(String($rootScope.datos.cedula).indexOf(".") >= 0 || String($rootScope.datos.cedula).indexOf(",") >= 0){
-                $scope.mostrarAyuda("Inicio de sesión","Ingresa sólo números");
-                return;
-            }
+    //Autenticar a la Mamá Empresaria
+    $scope.capturarCedula = function() {
 
-            if(Internet.get()){
+        alert("done");
 
-                $scope.loading =  $ionicLoading.show({
-                        template: Utilidades.getPlantillaEspera('Iniciando sesión')
+        $rootScope.datos = { cedula: $scope.datosInicio.cedula }
+
+        //Cédula vacía
+        if(!$rootScope.datos.cedula){
+            $scope.mostrarAyuda("Inicio de sesión","Ingresa tu cédula");
+            return;
+        }
+
+        //Cantidad de caracteres
+        if(String($rootScope.datos.cedula).length < 6 || String($rootScope.datos.cedula).length > 10){
+            $scope.mostrarAyuda("Inicio de sesión","Ingresa entre 6 y 10 dígitos");
+            return;
+        }
+
+        //Caracteres especiales
+        if(String($rootScope.datos.cedula).indexOf(".") >= 0 || String($rootScope.datos.cedula).indexOf(",") >= 0){
+            $scope.mostrarAyuda("Inicio de sesión","Ingresa sólo números");
+            return;
+        }
+
+        if(Internet.get()){
+
+            $scope.loading =  $ionicLoading.show({
+                template: Utilidades.getPlantillaEspera('Iniciando sesión')
+            });
+
+            Mama.autenticar($rootScope.datos.cedula, $rootScope, $http, $filter, Mama, function(success, mensajeError, data){
+
+                $ionicLoading.hide();
+
+                if(success){
+
+                    var irABienvenida = !(localStorage && localStorage.nombre);
+
+                    //Almacenar la cédula si hay almacenamiento local
+                    if(localStorage ){
+
+                        localStorage.cedula = $scope.datosInicio.cedula;
+                        localStorage.nombre = $rootScope.datos.nombre;
+                        localStorage.segmento = $rootScope.datos.segmento;
+                    }
+
+                    $scope.datosInicio = {cedula: '' };
+
+                    $ionicHistory.nextViewOptions({
+                        disableBack: true
                     });
 
-            	Mama.autenticar($rootScope.datos.cedula, $rootScope, $http, $filter, Mama, function(success, mensajeError, data){
+                    if(irABienvenida){
+                        $state.go('app.bienvenida');
+                    }else{
+                        $state.go('app.menu.tabs.home');
+                    }
 
-                    $ionicLoading.hide();
-                    
-                    if(success){
-                                
-                            var irABienvenida = !(localStorage && localStorage.nombre);
 
-                            //Almacenar la cédula si hay almacenamiento local
-                            if(localStorage ){
-                                
-                                localStorage.cedula = $scope.datosInicio.cedula;
-                                localStorage.nombre = $rootScope.datos.nombre;
-                                localStorage.segmento = $rootScope.datos.segmento;
-                            }
-                            
-                            $scope.datosInicio = {cedula: '' };
-                            
-                            $ionicHistory.nextViewOptions({
-                             disableBack: true
-                            });
-                            
-                            if(irABienvenida){
-                               $state.go('app.bienvenida');
-                            }else{
-                               $state.go('app.menu.tabs.home');
-                            }
-                           
-                            
-                        }else{
-                           $scope.mostrarAyuda("Inicio de sesión", mensajeError);
-                        }
+                }else{
+                    $scope.mostrarAyuda("Inicio de sesión", mensajeError);
+                }
 
-            	});
-            	
-            }else{
-                $scope.mostrarAyuda("Inicio de sesión","Por favor verifica tu conexión a internet");
-            }
+            });
+
+        }else{
+            $scope.mostrarAyuda("Inicio de sesión","Por favor verifica tu conexión a internet");
         }
-    });
+    }
+});
