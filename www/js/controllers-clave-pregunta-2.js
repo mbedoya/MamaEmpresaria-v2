48129d1a56c16ec9a3159a21717855779e5a9cfd
mbedoya <mauricio.bedoya@gmail.com>
2015-06-24 16:52:49 -0500

    confirmacion de opciones

--- www/js/controllers-clave-pregunta-2.js
+++ www/js/controllers-clave-pregunta-2.js
@@ -10,8 +10,59 @@ moduloControlador.controller('ClavePregunta2Ctrl', function($scope, $location, $
         });
     };
 
+    $scope.respuestaSeleccionada = function(respuesta){
+        $scope.respuesta = respuesta;
+    };
+
+    $scope.confirmar = function() {
+
+        var myPopup = $ionicPopup.show({
+            template: 'Mamá, elegiste la opción "' + $scope.respuesta + '" ¿Es correcto?',
+            title: 'Creación de clave',
+            subTitle: '',
+            scope: $scope,
+            buttons: [
+                { text: 'No',
+                    onTap: function (e) {
+                        console.log(e);
+                        return false;
+                    }
+                },
+                {
+                    text: '<b>Si</b>',
+                    type: 'button-positive',
+                    onTap: function (e) {
+                        console.log(e);
+                        return true;
+                    }
+                }
+            ]
+        });
+        myPopup.then(function (res) {
+
+            if(res){
+
+                if(Internet.get()){
+
+                    var respuestaValida = true;
+
+                    if(respuestaValida){
+                        $location.path('/app/clave-nueva-clave-1');
+                    }else{
+                        $scope.mostrarAyuda("Creación de clave", "No hemos pedido validar correctamente tu información, uno de nuestros asesores te estará contactando en XXX");
+                    }
+
+                }else{
+                    $scope.mostrarAyuda("Creación de clave","Por favor verifica tu conexión a internet");
+                }
+            }
+
+        });
+    };
+
     $scope.inicializar = function() {
 
+        $scope.respuesta = "";
         $scope.respuestas = new Array();
 
         Mama.getRespuestasPregunta2(function (success, data) {
@@ -26,26 +77,6 @@ moduloControlador.controller('ClavePregunta2Ctrl', function($scope, $location, $
 
     $scope.continuar = function() {
 
-        try{
-
-            if(Internet.get()){
-
-                var respuestaValida = true;
-
-                if(respuestaValida){
-                    $location.path('/app/clave-nueva-clave-1');
-                }else{
-                    $scope.mostrarAyuda("Creación de clave", "No hemos pedido validar correctamente tu información, uno de nuestros asesores te estará contactando en XXX");
-                }
-
-            }else{
-                $scope.mostrarAyuda("Creación de clave","Por favor verifica tu conexión a internet");
-            }
-
-        }catch (err){
-            alert(err.message);
-        }
-
-
+        $scope.confirmar();
     }
 });
