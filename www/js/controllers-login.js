55fdbace44d673283dc6f2d6955f1f702fb6f5be
Mauricio <mauricio.bedoya@gmail.com>
2015-06-25 09:31:02 -0500

    versión para Mamás Piloto

--- www/js/controllers-login.js
+++ www/js/controllers-login.js
@@ -47,33 +47,7 @@ moduloControlador.controller('LoginCtrl', function($scope, $location, $rootScope
                     template: Utilidades.getPlantillaEspera('Iniciando sesión')
                 });
 
-
-                Mama.validarCedula(function(success, data){
-
-                    $ionicLoading.hide();
-
-                    if(success){
-
-                        if(data.razonRechazo){
-                            $scope.mostrarAyuda("Inicio de sesión", data.razonRechazo);
-                        }else{
-                            if(data.bloqueoME && data.bloqueoME == "1"){
-                                $scope.mostrarAyuda("Inicio de sesión", "Para Novaventa será un placer tenerte de nuevo con nosotros, para poderte activar por favor comunícate con la línea " + $rootScope.lineaAtencion);
-                            }else{
-
-                                if(data.estadoPastDue && Number(data.estadoPastDue) >= 4){
-
-                                    $scope.mostrarAyuda("Inicio de sesión","Hola Mamá, tienes un saldo pendiente por pagar de " + filter('currency')(data.saldoBalance, '$', 0) + '. Te invitamos a ponerte al día. Ten presente que este valor puede aumentar debido a los gatos adicionales que se presentan por la entidad de cobro.');
-
-                                }else{
-
-                                    if(data.tieneClave  && data.tieneClave == "1"){
-
-                                        $scope.loading =  $ionicLoading.show({
-                                            template: Utilidades.getPlantillaEspera('Autenticando')
-                                        });
-
-                                        Mama.autenticar($rootScope.datos.cedula, $rootScope, $http, $filter, Mama, function(success, mensajeError, data){
+Mama.autenticar($rootScope.datos.cedula, $rootScope, $http, $filter, Mama, function(success, mensajeError, data){
 
                                             $ionicLoading.hide();
 
@@ -106,23 +80,7 @@ moduloControlador.controller('LoginCtrl', function($scope, $location, $rootScope
                                             }
 
                                         });
-
-                                    }else{
-
-                                        //Ir a la creación de la clave
-                                        $location.path('/app/clave-pregunta-1');
-                                    }
-
-                                }
-
-                            }
-                        }
-
-                    }else{
-                        $scope.mostrarAyuda("Inicio de sesión", "Lo sentimos, no es posible iniciar sesión en este momento");
-                    }
-
-                });
+                
 
 
             }else{
