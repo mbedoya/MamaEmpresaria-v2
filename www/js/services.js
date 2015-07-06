63cceeb114a4798124fbc57187c82eabe5fac018
mbedoya <mauricio.bedoya@gmail.com>
2015-07-06 17:01:41 -0500

    ingreso de clave para inicio de sesión

--- www/js/services.js
+++ www/js/services.js
@@ -425,8 +425,8 @@ angular.module('novaventa.services', [])
                 
                 //Cadena en Base 64 usuario:clave
                 var cadenaBase64 = btoa(cedula + ":" + rootScope.datos.clave);
-                var urlValidacion = rootScope.configuracion.ip_servidores +  "/AntaresWebServices/interfaceAntares/validacionAntares/" + cedula +"/1";
-                //var urlValidacion = rootScope.configuracion.ip_servidores +  "/AntaresWebServices/interfaceAntares/validacionAntares";
+                //var urlValidacion = rootScope.configuracion.ip_servidores +  "/AntaresWebServices/interfaceAntares/validacionAntares/" + cedula +"/1";
+                var urlValidacion = rootScope.configuracion.ip_servidores +  "/AntaresWebServices/interfaceAntares/validacionAntares";
                 
                 var req = {
                     method: 'GET',
