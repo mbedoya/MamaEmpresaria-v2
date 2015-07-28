dc50744c4506ee84605f9b4ae7c564988e07c250
Mauricio <mauricio.bedoya@gmail.com>
2015-07-28 15:54:02 -0500

    correccion autenticacion intentos

--- www/js/services.js
+++ www/js/services.js
@@ -809,13 +809,13 @@ angular.module('novaventa.services', [])
 
                             }else{
 
-                                if(data.intentosLogin == 5){
+                                if(data.intentos == 5){
 
                                     mensajeError = "Lo sentimos, tu clave no es válida para la cédula ingresada. Deseas ayuda?";
                                     data.mostrarSolicitudAyuda = true;
 
                                 }else{
-                                    if(data.intentosLogin == 10){
+                                    if(data.intentos == 10){
 
                                         mensajeError = "Lo sentimos, tu clave no es válida para la cédula ingresada. Nuestros asesores de servicios te estarán contactando";
                                         data.enviarSolicitudAsesor = true;
