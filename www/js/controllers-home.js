f303f5a1efc20af47fb5552f926756175b7fc34c
sebas0126 <sebas0126@gmail.com>
2016-01-26 16:31:18 -0500

    Alert popup

--- www/js/controllers-home.js
+++ www/js/controllers-home.js
@@ -15,7 +15,7 @@ moduloControlador.controller('HomeCtrl', function($scope, $rootScope, $state, $i
         
         var notificationOpenedCallback = function(jsonData) {
             //console.log('didReceiveRemoteNotificationCallBack: ' + JSON.stringify(jsonData));
-            //alert(jsonData.additionalData.title+"\n\n"+jsonData.message);   
+            alert(jsonData.additionalData.title+"\n\n"+jsonData.message);   
             var alertPopup = $ionicPopup.alert({
                 title: jsonData.additionalData.title,
                 template: jsonData.message
