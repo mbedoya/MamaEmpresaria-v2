844b36dff45ed9a88d2ab9fbcd8ae376823b4308
mbedoya <mauricio.bedoya@gmail.com>
2016-04-13 20:48:42 -0500

    ajuste Chat, paso datos a Iframe

--- www/js/controllers-chat.js
+++ www/js/controllers-chat.js
@@ -3,4 +3,9 @@ moduloControlador.controller('ChatCtrl', function($scope, $rootScope) {
     //Registro en Analytics
     GA.trackPage($rootScope.gaPlugin, "Chat");
 
+    $scope.getRutaIframe = function(){
+        return "http://laboru.co/zopim?cedula=" + $rootScope.datos.cedula +
+            "&nombre=" + $rootScope.datos.nombre + "&segmento=" $rootScope.datos.segmento;
+    }
+
 });
