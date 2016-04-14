1b43a860039bd72650259eaaf02e7c48035ddf5b
mbedoya <mauricio.bedoya@gmail.com>
2016-04-13 21:16:47 -0500

    se elimina restricción de seguridad de angular para iframe

--- www/js/controllers-chat.js
+++ www/js/controllers-chat.js
@@ -1,11 +1,12 @@
-moduloControlador.controller('ChatCtrl', function($scope, $rootScope) {
+moduloControlador.controller('ChatCtrl', function($scope, $rootScope, $sce, GA) {
 
     //Registro en Analytics
     GA.trackPage($rootScope.gaPlugin, "Chat");
 
     $scope.getRutaIframe = function(){
-        return "http://laboru.co/zopim?cedula=" + $rootScope.datos.cedula +
-            "&nombre=" + $rootScope.datos.nombre + "&segmento=" + $rootScope.datos.segmento;
+        var ruta = $sce.trustAsResourceUrl("http://laboru.co/zopim?cedula=" + $rootScope.datos.cedula +
+            "&nombre=" + $rootScope.datos.nombre + "&segmento=" + $rootScope.datos.segmento);
+        return ruta;
     }
 
 });
