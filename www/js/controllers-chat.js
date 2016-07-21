b40156ed95ad098561629f946dcf19c8d952fb87
mbedoya <mauricio.bedoya@gmail.com>
2016-07-21 09:24:11 -0500

    Prueba jivo

--- www/js/controllers-chat.js
+++ www/js/controllers-chat.js
@@ -4,8 +4,15 @@ moduloControlador.controller('ChatCtrl', function($scope, $rootScope, $sce, GA) 
     GA.trackPage($rootScope.gaPlugin, "Chat");
 
     $scope.getRutaIframe = function(){
+
+        //var ruta = 'http://www.google.com';
+        var ruta = $sce.trustAsResourceUrl('http://laboru.co/jivo/index.html');
+
+        console.log(ruta);
+
+        /*
         var ruta = $sce.trustAsResourceUrl("http://mechat.site88.net?cedula=" + $rootScope.datos.cedula +
-            "&nombre=" + $rootScope.datos.nombre + "&segmento=" + $scope.datos.segmento);
+            "&nombre=" + $rootScope.datos.nombre + "&segmento=" + $scope.datos.segmento); */
         return ruta;
     }
 
