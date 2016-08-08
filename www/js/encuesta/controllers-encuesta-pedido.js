bfa5de207f2a0690a2a546498ac52165c1880eb5
mbedoya <mauricio.bedoya@gmail.com>
2016-08-08 11:56:34 -0500

    Encuesta con "atrás"

--- www/js/encuesta/controllers-encuesta-pedido.js
+++ www/js/encuesta/controllers-encuesta-pedido.js
@@ -1,86 +1,125 @@
-moduloControlador.controller('EncuestaPedidoCtrl', function($scope, $location, $rootScope, $ionicLoading, $ionicPopup, $state, $filter, $ionicHistory, $ionicScrollDelegate, Mama, Internet, GA, Encuesta, Utilidades) {
+moduloControlador.controller('EncuestaPedidoCtrl', function ($scope, $location, $rootScope, $ionicLoading, $ionicPopup, $state, $filter, $ionicHistory, $ionicScrollDelegate, Mama, Internet, GA, Encuesta, Utilidades) {
 
     //Registro en Analytics
     GA.trackPage($rootScope.gaPlugin, "Encuesta Pedido");
 
-    $scope.mostrarAyuda = function(titulo, mensaje) {
+    $scope.mostrarAyuda = function (titulo, mensaje) {
         var alertPopup = $ionicPopup.alert({
             title: "",
             template: mensaje
         });
     };
 
-    $scope.irAtras = function() {
+    $scope.irAtras = function () {
         if ($scope.indice > 0) {
             $scope.indice = $scope.indice - 1;
+
+            $scope.actualizarValoresRespuestas();
         }
     }
 
-    $scope.mostrarAtras = function() {
+    $scope.mostrarAtras = function () {
         return $scope.indice > 0;
     }
 
-    $scope.continuar = function() {
+    $scope.actualizarValoresRespuestas = function () {
 
-        console.log("Indice respuesta:" + $scope.indiceRespuesta);
-        console.log("Cerrada simple" + $scope.EsPreguntaCerradaSimple());
+        //Respuesta Abierta
+        $scope.respuestaTexto = { valor: '' };
+        //Respuesta Multiple Cerrada
+        $scope.respuestaMultipleCerrada = { valor: null };
+        //Respuesta Multiple Varias Selecciones
+        $scope.respuestaMultipleMultiple = { valor: new Array() };
+
+        if ($scope.indice > -1) {
+
+            if ($scope.EsPreguntaCerradaSimple() && $scope.preguntaContestada()) {
+                $scope.respuestaMultipleCerrada = { valor: $scope.preguntaContestada()[0] };
+            } else {
+
+                if ($scope.EsPreguntaAbierta() && $scope.preguntaContestada()) {
+                    $scope.respuestaTexto = { valor: $scope.preguntaContestada()[0] };
+                } else {
+
+                    for (var index = 0; index < $scope.obtenerPregunta().posiblesRespuestas.length; index++) {
+                        var element = $scope.obtenerPregunta().posiblesRespuestas[index];
+
+                        var respTemp = $scope.preguntaContestada();
+                        if (respTemp) {
+                            var found = false;
+                            for (var i = 0; i < respTemp.length; i++) {
+                                var e = respTemp[i];
+                                if(e == element.posibleRespuesta){
+                                    found = true;
+                                    $scope.respuestaMultipleMultiple.valor.push(true);
+                                    break;
+                                }
+                            }
+                            if(!found){
+                                $scope.respuestaMultipleMultiple.valor.push(false);
+                            }
+                        } else {
+                            $scope.respuestaMultipleMultiple.valor.push(false);
+                        }
+
+                    }
+
+                    console.log($scope.respuestaMultipleMultiple);
+
+                }
+
+            }
+        }
+    }
+
+    $scope.continuar = function () {
 
         //Validación de ingreso de respuesta cerrada única
-        if($scope.indiceRespuesta == -1 && $scope.EsPreguntaCerradaSimple()){
-            $scope.mostrarAyuda("","Debes seleccionar una opción");
+        if (!$scope.preguntaContestada() && $scope.EsPreguntaCerradaSimple()) {
+            $scope.mostrarAyuda("", "Debes seleccionar una opción");
             return;
         }
 
         //Validación de ingreso de respuesta cerrada múltiple
-        if($scope.indiceRespuesta == -1  && $scope.EsPreguntaCerradaMultiple()){
-            $scope.mostrarAyuda("","Debes seleccionar mínimo una opción");
+        if ($scope.indiceRespuesta == -1 && $scope.EsPreguntaCerradaMultiple()) {
+            $scope.mostrarAyuda("", "Debes seleccionar mínimo una opción");
             return;
         }
 
         //Validación de ingreso de respuesta cerrada múltiple
-        if(String($scope.respuestaTexto.valor).trim().length == 0 && $scope.EsPreguntaAbierta()){
-            $scope.mostrarAyuda("","Por favor contesta la pregunta");
+        if (String($scope.respuestaTexto.valor).trim().length == 0 && $scope.EsPreguntaAbierta()) {
+            $scope.mostrarAyuda("", "Por favor contesta la pregunta");
             return;
         }
 
-        //Obtener las respuestas dadas por la Mamá
-        var respuestas = new Array();
-        if($scope.EsPreguntaCerradaSimple() || $scope.EsPreguntaCerradaMultiple()){
-            respuestas.push($scope.obtenerPregunta().posiblesRespuestas[$scope.indiceRespuesta].posibleRespuesta);
-        }else{
-            respuestas.push($scope.respuestaTexto.valor);
-        }
-
-        //Crear el objeto de respuesta completo
-        $scope.respuestas.push({ pregunta: $scope.obtenerPregunta().pregunta, respuestas: respuestas });
-
-        $scope.indiceRespuesta = -1;
-        $scope.respuestaTexto.valor = "";
-
-        if($scope.indice + 1 < $scope.preguntas.length){
+        if ($scope.indice + 1 < $scope.preguntas.length) {
             $scope.indice++;
             $ionicScrollDelegate.scrollTop();
-        }else{
 
-            $scope.loading =  $ionicLoading.show({
+            //Actualizar la respuesta
+            $scope.actualizarValoresRespuestas();
+
+        } else {
+
+            $scope.loading = $ionicLoading.show({
                 template: Utilidades.getPlantillaEspera('Enviando las respuestas de la Encuesta')
             });
 
-            var objetoRespuesta = { 
+            var objetoRespuesta = {
                 preguntas: $scope.respuestas
-             };
+            };
 
-            Encuesta.enviarRespuestasEncuesta(objetoRespuesta ,function(success, data) {
+            Encuesta.enviarRespuestasEncuesta(objetoRespuesta, function (success, data) {
 
                 $ionicLoading.hide();
 
                 if (success) {
                     console.log("Encuesta enviada");
-                }else{
+                } else {
                     console.log("Error al enviar encuesta");
                 }
 
-                $scope.mostrarAyuda("Inicio de sesión","Encuesta finalizada! Muchas gracias por tu participación");
+                $scope.mostrarAyuda("Inicio de sesión", "Encuesta finalizada! Muchas gracias por tu participación");
                 $location.path('/app/menu/tabs/home');
 
             });
@@ -90,75 +129,136 @@ moduloControlador.controller('EncuestaPedidoCtrl', function($scope, $location, $
 
     $scope.contestarPregunta = function (indice) {
         $scope.indiceRespuesta = indice;
+
+        if ($scope.EsPreguntaCerradaSimple()) {
+            $scope.respuestas[$scope.indice].respuestas = new Array();
+            $scope.respuestas[$scope.indice].respuestas.push($scope.respuestaMultipleCerrada.valor);
+        }
+
+        console.log($scope.respuestas);
     }
 
-    $scope.contestarPreguntaTexto = function () {
-        
+    $scope.contestarPreguntaAbierta = function () {
+        $scope.respuestas[$scope.indice].respuestas = new Array();
+        $scope.respuestas[$scope.indice].respuestas.push($scope.respuestaTexto.valor);
     }
 
-    $scope.obtenerPregunta = function(){
-        if($scope.indice == -1){
+    $scope.contestarPreguntaMultiple = function (indice) {
+
+        console.log($scope.obtenerPregunta().posiblesRespuestas[indice].posibleRespuesta);
+
+        if(!$scope.preguntaContestada()){
+            $scope.respuestas[$scope.indice].respuestas = new Array();
+        }
+
+        var encontrado = false;
+        var indiceEncontrado;
+        //Buscar si no hay sido contestada, si ya lo ha sido entonces eliminarla, de lo contrario adicionarla
+        for (var index = 0; index < $scope.preguntaContestada().length; index++) {
+            var element = $scope.preguntaContestada()[index];
+            
+            if(element == $scope.obtenerPregunta().posiblesRespuestas[indice].posibleRespuesta){
+                encontrado = true;
+                indiceEncontrado = index;
+                break;
+            }
+        }
+
+        if (encontrado) {
+            $scope.respuestas[$scope.indice].respuestas.splice(indiceEncontrado, 1);
+            $scope.respuestaMultipleMultiple.valor[indice] = false;
+        }else{
+            $scope.respuestas[$scope.indice].respuestas.push($scope.obtenerPregunta().posiblesRespuestas[indice].posibleRespuesta);
+            $scope.respuestaMultipleMultiple.valor[indice] = true;
+        }
+
+        console.log($scope.respuestas);
+    }
+
+    $scope.preguntaContestada = function () {
+        return $scope.respuestas[$scope.indice].respuestas;
+    }
+
+    $scope.obtenerPregunta = function () {
+        if ($scope.indice == -1) {
             return null;
         }
 
         return $scope.preguntas[$scope.indice];
     }
 
-    $scope.EsPreguntaCerradaMultiple = function(){
+    $scope.EsPreguntaCerradaMultiple = function () {
 
         return $scope.preguntas && $scope.preguntas.length > 0 &&
-            $scope.preguntas[$scope.indice].respuestaMultiple && 
+            $scope.preguntas[$scope.indice].respuestaMultiple &&
             $scope.preguntas[$scope.indice].posiblesRespuestas && $scope.preguntas[$scope.indice].posiblesRespuestas.length > 0;
 
     }
 
-    $scope.EsPreguntaCerradaSimple = function(){
+    $scope.EsPreguntaCerradaSimple = function () {
 
         return $scope.preguntas && $scope.preguntas.length > 0 &&
-            !$scope.preguntas[$scope.indice].respuestaMultiple && 
+            !$scope.preguntas[$scope.indice].respuestaMultiple &&
             $scope.preguntas[$scope.indice].posiblesRespuestas && $scope.preguntas[$scope.indice].posiblesRespuestas.length > 0;
     }
 
-    $scope.EsPreguntaAbierta = function(){
+    $scope.EsPreguntaAbierta = function () {
 
         return $scope.preguntas && $scope.preguntas.length > 0 &&
             !$scope.preguntas[$scope.indice].respuestaMultiple &&
             (!$scope.preguntas[$scope.indice].posiblesRespuestas || $scope.preguntas[$scope.indice].posiblesRespuestas.length == 0);
     }
 
-    $scope.inicializar = function() {
+    $scope.crearVectorRespuestas = function () {
+
+        for (var index = 0; index < $scope.preguntas.length; index++) {
+            var element = $scope.preguntas[index];
+
+            //Crear el objeto de respuesta completo
+            $scope.respuestas.push({ pregunta: element.pregunta, respuestas: null });
+        }
+
+        console.log($scope.respuestas);
+    }
+
+    $scope.inicializar = function () {
 
         $scope.respuestas = new Array();
+
         $scope.indiceRespuesta = -1;
-        $scope.respuestaTexto = { valor: ''};
         $scope.indice = -1;
 
-        if(Internet.get()){
+        $scope.actualizarValoresRespuestas();
 
-            $scope.loading =  $ionicLoading.show({
+        if (Internet.get()) {
+
+            $scope.loading = $ionicLoading.show({
                 template: Utilidades.getPlantillaEspera('Consultando la Encuesta')
             });
 
-            Encuesta.obtenerPreguntasEncuesta(function(success, data) {
+            Encuesta.obtenerPreguntasEncuesta(function (success, data) {
 
                 $ionicLoading.hide();
 
                 if (success) {
                     $scope.preguntas = data.preguntas;
                     $scope.indice = 0;
+
+                    $scope.crearVectorRespuestas();
+
                     console.log($scope.preguntas);
-                }else{
+                } else {
                     $location.path('/app/menu/tabs/home');
-                    $scope.mostrarAyuda("Inicio de sesión","No es posible cargar la Encuesta");
+                    $scope.mostrarAyuda("Inicio de sesión", "No es posible cargar la Encuesta");
                 }
             });
-        }else{
-            $scope.mostrarAyuda("Inicio de sesión","Por favor verifica tu conexión a internet, no ha sido posible enviar la solicitud de contacto");
+        } else {
+            $scope.mostrarAyuda("Inicio de sesión", "Por favor verifica tu conexión a internet, no ha sido posible enviar la solicitud de contacto");
         }
     }
 
-    $scope.$on('$ionicView.beforeEnter', function(){
+    $scope.$on('$ionicView.beforeEnter', function () {
         $scope.inicializar();
     });
-    
+
 });
