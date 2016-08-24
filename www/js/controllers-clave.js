eb74a8dbebeacdd73eea3704d323e482ba1805b4
mbedoya <mauricio.bedoya@gmail.com>
2016-08-24 11:37:49 -0500

    se consultan las notificaciones al iniciar sesión desde ingreso de clave

--- www/js/controllers-clave.js
+++ www/js/controllers-clave.js
@@ -1,39 +1,39 @@
-moduloControlador.controller('ClaveCtrl', function($scope, $location, $rootScope, $ionicLoading, $ionicPopup, $state, $http, $filter, $ionicHistory, Mama, Internet, GA, Utilidades) {
+moduloControlador.controller('ClaveCtrl', function ($scope, $location, $rootScope, $ionicLoading, $ionicPopup, $state, $http, $filter, $ionicHistory, Mama, Internet, GA, Utilidades, Notificaciones) {
 
     //Registro en Analytics
     GA.trackPage($rootScope.gaPlugin, "Clave");
 
-    $scope.mostrarAyuda = function(titulo, mensaje) {
+    $scope.mostrarAyuda = function (titulo, mensaje) {
         var alertPopup = $ionicPopup.alert({
             title: "",
             template: mensaje
         });
     };
 
-    $scope.limpiar = function(){
-        $scope.modelo = { clave: ''};
+    $scope.limpiar = function () {
+        $scope.modelo = { clave: '' };
     }
 
-    $scope.irARecuperarClave = function(){
+    $scope.irARecuperarClave = function () {
         $rootScope.recuperarClave = true;
         $location.path('/app/clave-pregunta-1');
     }
 
-    $scope.enviarSolicitudContacto = function(){
+    $scope.enviarSolicitudContacto = function () {
         //Enviar solicitus de contacto
 
-        if(Internet.get()){
-            Mama.solicitarContactoAsesor("INTENTOS_FALLIDOS", function(success, data) {
+        if (Internet.get()) {
+            Mama.solicitarContactoAsesor("INTENTOS_FALLIDOS", function (success, data) {
                 if (success) {
 
                 }
             });
-        }else{
-            $scope.mostrarAyuda("Inicio de sesión","Por favor verifica tu conexión a internet, no ha sido posible enviar la solicitud de contacto");
+        } else {
+            $scope.mostrarAyuda("Inicio de sesión", "Por favor verifica tu conexión a internet, no ha sido posible enviar la solicitud de contacto");
         }
     }
 
-    $scope.confirmar = function(mensaje) {
+    $scope.confirmar = function (mensaje) {
 
         var myPopup = $ionicPopup.show({
             template: mensaje,
@@ -41,11 +41,12 @@ moduloControlador.controller('ClaveCtrl', function($scope, $location, $rootScope
             subTitle: '',
             scope: $scope,
             buttons: [
-                { text: 'No',
-                 onTap: function (e) {
-                     console.log(e);
-                     return false;
-                 }
+                {
+                    text: 'No',
+                    onTap: function (e) {
+                        console.log(e);
+                        return false;
+                    }
                 },
                 {
                     text: '<b>Si</b>',
@@ -59,9 +60,9 @@ moduloControlador.controller('ClaveCtrl', function($scope, $location, $rootScope
         });
 
         myPopup.then(function (res) {
-            if(res){
+            if (res) {
                 $scope.enviarSolicitudContacto();
-            }else{
+            } else {
 
             }
 
@@ -69,68 +70,85 @@ moduloControlador.controller('ClaveCtrl', function($scope, $location, $rootScope
     };
 
     //Autenticar a la Mamá Empresaria
-    $scope.continuar = function() {
+    $scope.continuar = function () {
 
         var clave = $("#txtClaveLogin").val();
 
         //alert($("#txtClave").val());
 
         //Cédula vacía
-        if(!clave || clave.length == 0){
-            $scope.mostrarAyuda("Inicio de sesión","Ingresa tu clave");
+        if (!clave || clave.length == 0) {
+            $scope.mostrarAyuda("Inicio de sesión", "Ingresa tu clave");
             return;
         }
 
         //Cantidad de caracteres
-        if(clave.length != 4){
-            $scope.mostrarAyuda("Inicio de sesión","Ingresa 4 dígitos");
+        if (clave.length != 4) {
+            $scope.mostrarAyuda("Inicio de sesión", "Ingresa 4 dígitos");
             return;
         }
 
         //Cantidad de caracteres
-        if(clave.length > 4){
-            $scope.mostrarAyuda("Inicio de sesión","Ingresa únicamente 4 dígitos");
+        if (clave.length > 4) {
+            $scope.mostrarAyuda("Inicio de sesión", "Ingresa únicamente 4 dígitos");
             return;
         }
 
         //Caracteres especiales
-        if(clave.indexOf(".") >= 0 || clave.indexOf(",") >= 0){
-            $scope.mostrarAyuda("Inicio de sesión","Ingresa sólo números");
+        if (clave.indexOf(".") >= 0 || clave.indexOf(",") >= 0) {
+            $scope.mostrarAyuda("Inicio de sesión", "Ingresa sólo números");
             return;
         }
 
-        try{
+        try {
 
-            if(Internet.get()){
+            if (Internet.get()) {
 
-                $scope.loading =  $ionicLoading.show({
+                $scope.loading = $ionicLoading.show({
                     template: Utilidades.getPlantillaEspera('Validando clave')
                 });
 
                 $rootScope.datos.clave = clave;
 
-                Mama.autenticar($rootScope.datos.cedula, $rootScope, $http, $filter, Mama, function(success, mensajeError, data){
+                Mama.autenticar($rootScope.datos.cedula, $rootScope, $http, $filter, Mama, function (success, mensajeError, data) {
 
                     $ionicLoading.hide();
 
                     //alert(success);
 
-                    if(success){
+                    if (success) {
 
-                        $scope.loading =  $ionicLoading.show({
+                        $scope.loading = $ionicLoading.show({
                             template: Utilidades.getPlantillaEspera('Iniciando sesión')
                         });
 
-                        Mama.getInformacionBasica(function(success, mensajeError){
+                        //Consultar las Notificaciones de Antares e Historial
+                        Notificaciones.consultar(function (success, historial, antaresNuevas) {
+                            if (success) {
+                                $rootScope.notificacionesHistorial = historial;
+                                $rootScope.notificacionesNuevas = antaresNuevas;
+
+                                //Si hay notificaciones nuevas hoy entonces mostrar la primera
+                                if (antaresNuevas && antaresNuevas.length > 0 &&
+                                    Utilidades.formatearFechaActual() == Utilidades.formatearFechaCadena(antaresNuevas[0].fecha)) {
+
+                                    Notificaciones.mostrarNotificacionNueva(antaresNuevas[0]);
+                                }
+                            } else {
+                                $rootScope.notificacionesHistorial = historial;
+                            }
+                        });
+
+                        Mama.getInformacionBasica(function (success, mensajeError) {
 
                             $ionicLoading.hide();
 
-                            if(success){
+                            if (success) {
 
                                 $scope.modelo.clave = '';
 
                                 //Almacenar datos si hay almacenamiento local
-                                if(localStorage){
+                                if (localStorage) {
 
                                     localStorage.cedula = $rootScope.datos.cedula;
                                     localStorage.nombre = $rootScope.datos.nombre;
@@ -138,39 +156,39 @@ moduloControlador.controller('ClaveCtrl', function($scope, $location, $rootScope
                                     localStorage.clave = $rootScope.datos.clave;
                                 }
 
-                                $scope.datosInicio = {clave: '' };
+                                $scope.datosInicio = { clave: '' };
 
                                 $ionicHistory.nextViewOptions({
                                     disableBack: true
                                 });
 
                                 //Si la Mamá tiene versión para aceptar entonces ir a terminos y condiciones
-                                if ($rootScope.datos.versionHabeasData){
+                                if ($rootScope.datos.versionHabeasData) {
                                     $rootScope.irAHomeLuegoTerminos = true;
                                     $location.path('/app/terminos-condiciones');
-                                }else{
+                                } else {
                                     $ionicHistory.nextViewOptions({
                                         historyRoot: true
                                     });
                                     $location.path('/app/menu/tabs/home');
                                 }
 
-                            }else{
+                            } else {
                                 $scope.mostrarAyuda("Creación de clave", mensajeError);
                             }
 
                         });
 
-                    }else{
+                    } else {
 
                         //alert(data.mostrarSolicitudAyuda);
 
-                        if(data.mostrarSolicitudAyuda){
+                        if (data.mostrarSolicitudAyuda) {
                             $scope.confirmar(mensajeError);
-                        }else{
+                        } else {
                             $scope.mostrarAyuda("Ingreso de clave", mensajeError);
 
-                            if(data.enviarSolicitudAsesor){
+                            if (data.enviarSolicitudAsesor) {
                                 $scope.enviarSolicitudContacto();
                             }
                         }
@@ -178,17 +196,17 @@ moduloControlador.controller('ClaveCtrl', function($scope, $location, $rootScope
 
                 });
 
-            }else{
-                $scope.mostrarAyuda("Inicio de sesión","Por favor verifica tu conexión a internet");
+            } else {
+                $scope.mostrarAyuda("Inicio de sesión", "Por favor verifica tu conexión a internet");
             }
 
-        }catch (err){
+        } catch (err) {
             alert(err.message);
         }
 
     }
 
-    $scope.inicializar = function(){
+    $scope.inicializar = function () {
         $scope.limpiar();
     }
 
